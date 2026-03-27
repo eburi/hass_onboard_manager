f30dbca082346dd9fc5540a69a931c3a3c5db824
Erich Buri <erich.buri@gmail.com>
2026-03-27 08:08:05 -0500

    Fix CI validation and test integration loading

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,6 +22,11 @@ def mock_roles() -> list[dict[str, str]]:
     ]
 
 
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations: None) -> None:
+    """Enable custom integrations for all tests."""
+
+
 @pytest.fixture
 def mock_users() -> dict[str, dict[str, Any]]:
     """Return a dict of test users."""
